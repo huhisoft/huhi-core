28e9ca3899d2f4859756e5c421f10ddfa576a74e
huynq <huhibrowser@gmail.com>
2020-11-28 12:17:37 +0700

    update RUST DEPS URL

--- script/rust_deps_config.py
+++ script/rust_deps_config.py
@@ -5,5 +5,5 @@
 
 # Version number and URL for pre-configured rust dependency package
 # e.g. rust_deps_mac_0.1.0.gz
-RUST_DEPS_PACKAGES_URL = "https://rust-pkg-huhi-core.s3.huhisoft.com"
+RUST_DEPS_PACKAGES_URL = "https://huhisoft.com/file"
 RUST_DEPS_PACKAGE_VERSION = "0.1.4"
